2ef29932872e12850a2ec777d17c321ad0af42ad
zhaowei.liu <zhaowei.liu@w-oasis.com>
2018-11-16 10:01:18 +0800

    增加注释

--- src/main/java/com/woasis/nettydemo/server/EchoServer.java
+++ src/main/java/com/woasis/nettydemo/server/EchoServer.java
@@ -18,26 +18,28 @@ public class EchoServer {
     }
 
     public static void main(String[] args) throws Exception{
-        new EchoServer(7000).start();
+        new EchoServer(7000).start();//设置端口并启动
     }
 
     public void start() throws Exception{
 
+        //创建EventLoopGroup
         NioEventLoopGroup group = new NioEventLoopGroup();
         try {
+            //创建ServerBootstrap
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(group)
-                    .channel(NioServerSocketChannel.class)
-                    .localAddress(new InetSocketAddress(port))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                    .channel(NioServerSocketChannel.class)//指定使用Nio的传输channel
+                    .localAddress(new InetSocketAddress(port))//设置socke地址所选用的端口
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//添加EchoServerHandler到Channel的ChannelPipeline
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new EchoServerHandler());
                         }
                     });
-            ChannelFuture channelFuture = serverBootstrap.bind().sync();
-            channelFuture.channel().closeFuture().sync();
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();//绑定服务器
+            channelFuture.channel().closeFuture().sync();//关闭channel和块
         }finally {
-            group.shutdownGracefully().sync();
+            group.shutdownGracefully().sync();//关闭EventLoopGroup，释放资源
         }
     }
 }
